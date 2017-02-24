9b5c59fe37d06a55f99b8aec4a2fb8bc63510221
Filip <Filip@JARVIS>
2017-02-24 22:54:23 +0100

    Jasper Report

--- report-parent/report/src/main/java/com/home/report/MainReport.java
+++ report-parent/report/src/main/java/com/home/report/MainReport.java
@@ -16,6 +16,7 @@ public class MainReport {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        System.out.println("JASPER REPORTS");
     }
     
 }
